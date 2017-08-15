198326736dbe5a9378ee97ba3aeed0f0f4c38c59
ilonamor <ilonapl@president.org.ua>
2017-08-15 10:15:50 +0300

    first commit

---
+++ git1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace git1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, github!!!");
+
+            File.Create(".gitignore");
+        }
+    }
+}
